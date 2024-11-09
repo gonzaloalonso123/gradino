63de994a736014701b450e7e59664aec789d1356
1998KB <klajdibeqiraj88@gmail.com>
2024-11-09 17:00:14 +0100

    req

--- src/server/firestore.js
+++ src/server/firestore.js
@@ -1,7 +1,13 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "./config";
 
-export const addReservation = async (reservation) => {
-  const docRef = await addDoc(collection(db, "reservations"), reservation);
-  console.log("Document written with ID: ", docRef.id);
-};
+export async function addReservation(reservation) {
+  try {
+    const docRef = await addDoc(collection(db, "reservation"), reservation);
+    console.log("Reservation added with ID: ", docRef.id);
+    return docRef.id;
+  } catch (error) {
+    console.error("Error adding reservation: ", error);
+    throw new Error("Failed to add reservation");
+  }
+}
