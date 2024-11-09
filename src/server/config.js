253082fc3c98fa34833a4e0389ddc908ccccf446
1998KB <klajdibeqiraj88@gmail.com>
2024-11-09 16:54:03 +0100

    firestore

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDIsSkYXT_yzwo5wH8E9_7iIRohyUloUuU",
@@ -12,3 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { db };
